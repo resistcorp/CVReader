d961740fd20624887ff357073e6bc3ebbfffdf50
Boris Evieux <boris@resistcorp.org>
2016-04-10 21:25:53 +0200

    added a test overlay, and debugged

--- app/read/read.js
+++ app/read/read.js
@@ -19,7 +19,7 @@
 			name = $routeParams.CVName,
 			chain = $q.when(SData).then(
 				function(_standard){
-					angular.extend(myData, _standard.data);
+					angular.merge(myData, _standard.data);
 					$scope.serialized = JSON.stringify(myData);
 					$rootScope.progress = "..";
 				}
@@ -31,23 +31,26 @@
 		$scope.serialized = JSON.stringify(myData);
 		if(name != "standard"){
 			chain = chain.then(
-					$http.get('/CVData/' + name + ".json")
+					$http.get('./CVData/' + name + ".json")
 					.then(function _success(response) {
-						angular.extend(myData, response);
+						console.log("extending", myData, "with", response.data, response.data.head.title);
+						//overlay a new object over the standard CV
+						angular.merge(myData, response.data);
+						console.log("result :", myData, response.data);
 						$rootScope.progress = "...";
 						$scope.serialized = JSON.stringify(myData);
 					  }, function _error(response) {
 						$scope.serialized = JSON.stringify(myData);
 						$rootScope.progress = "...";
 						$rootScope.loading = false;
-					  })//overlay a new object over the standard CV
+					  })
 			);
 		}
 		$q.all([
 			$timeout(1200),//let the user enjoy my loader anim :D
 			chain
 		]).then(function(){
-			console.log(myData);
+			console.log("final data :",myData);
 			TreatCV(myData, $scope, $rootScope);
 		})
 	}
@@ -57,6 +60,66 @@
 		$rootScope.loading = false;
 		$scope.loading = false;
 		$scope.CVData = _data;
+		// sort skillset and techs, and expose them as an array
+		//they are stored as objects to be easily overwritten
+		for(let collection of [_data.body.SkillSet, _data.body.Techs]){
+			var array = [], i = 1;
+			for(var s in collection.sections){
+				var section = collection.sections[s];
+				if(isNaN(section.order))
+					section.order = 0.1 + i++;
+				section.id = s
+				array.push(section);
+			}
+			array.sort(compareSections);
+			collection.sortedSections = array;
+		}
+		//create one datapoint for every year of the timeline
+		//so that they can be displayed ( or not :) )
+		//One day I'll do it like this ♥
+		/*function* yearGen(){
+			var year = _data.body.timelineStart;
+			var end = _data.body.timelineEnd;
+			if(end == "now" || isNaN(end)){
+				end = new Date().year
+			}
+			if(year > end){
+				[year, end] = [end, year];
+			}
+			while(year <= end){
+				var ret = {
+					year : year
+				}
+				year++;
+				yield ret;
+			}
+		}
+		$scope.dates = [for (x of yearGen) x];*/
+		{
+			$scope.dates = [];
+			$scope.datesByYear = {};
+			var year = parseInt(_data.body.timelineStart);
+			var end = parseInt(_data.body.timelineEnd);
+			if(end == "now" || isNaN(end)){
+				end = new Date().getFullYear();
+			}
+			if(year > end){
+				[year, end] = [end, year];
+			}
+
+			for(;year <= end; year++){
+				var ret = {
+					year : year
+				}
+				$scope.datesByYear["y" + year] = ret;
+				$scope.dates.push(ret);
+			}
+		}
+
+		console.log("controller called", $scope, $scope.data, $scope.sections);
 		console.log(_data);
+	}
+	function compareSections(_a, _b){
+		return _a.order - _b.order;
 	}
 }
