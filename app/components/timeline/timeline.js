9610a1bcdbb3c6c43351fb9a325716f9955d5a36
Boris Evieux <boris@resistcorp.org>
2016-04-24 21:43:51 +0200

    started o nthe actual work experience displaying... needs refinement.

--- app/components/timeline/timeline.js
+++ app/components/timeline/timeline.js
@@ -1,14 +1,25 @@
 {//polluting is bad
 	function TimelineController($scope) {
 		console.log("timeline :", $scope);
+		$scope.getPeriodClass = getPeriodClass;
+		$scope.$watch(function(){
+			console.log("change in scope for timeline", JSON.stringify($scope.workPeriods, null, "  "), JSON.stringify($scope.dates, null, "  "));
+		})
+	}
+	function getPeriodClass(_period){
+		ret = ['period'];
+		if(_period.isPlaceHolder)
+			ret.push('placeholder');
+		return ret;
 	}
 
 	angular.module('CVReader.timeline', [])
 	.directive('cvTimeline',function(){
 	  return {
-	  	scope : {
+	  	/*scope : {
+	  		workPeriods : "=",
 	  		dates : "="
-	  	},
+	  	},*/
 		controller : ['$scope',
 		  TimelineController
 		],
--- app/read/read.js
+++ app/read/read.js
@@ -43,10 +43,10 @@
 			for (var i = 0; i < names.length; i++) {
 				var name = names[i];
 				if(overlays[name]){
-					console.log("extending", myData, "with", overlays[name], overlays[name].head.title);
-					//overlay a new object over the standard CV
+					//console.log("extending", myData, "with", overlays[name], overlays[name].head.title);
+					//overlay a new object over the current CV data
 					angular.merge(myData, overlays[name]);
-					console.log("result :", myData, overlays[name]);
+					//console.log("result :", myData, overlays[name]);
 					$rootScope.progress = "...";
 					$scope.serialized = JSON.stringify(myData);
 				}
@@ -102,29 +102,70 @@
 			}
 		}
 		$scope.dates = [for (x of yearGen) x];*/
+		var year = parseInt(_data.body.timelineStart);
+		var end = parseInt(_data.body.timelineEnd);
+		if(end == "now" || isNaN(end)){
+			end = new Date().getFullYear();
+		}
+		if(year > end){
+			[year, end] = [end, year];
+		}
 		{
 			$scope.dates = [];
+			$scope.workPeriods = [];
 			$scope.datesByYear = {};
-			var year = parseInt(_data.body.timelineStart);
-			var end = parseInt(_data.body.timelineEnd);
-			if(end == "now" || isNaN(end)){
-				end = new Date().getFullYear();
-			}
-			if(year > end){
-				[year, end] = [end, year];
-			}
 
 			for(;year <= end; year++){
 				var ret = {
-					year : year
+					year : year,
+					used : false
 				}
 				$scope.datesByYear["y" + year] = ret;
 				$scope.dates.push(ret);
 			}
 		}
-
-		console.log("controller called", $scope, $scope.data, $scope.sections);
-		console.log(_data);
+		{
+			var year, yFrom, yTo, sliceSize;
+			$scope.sliceSize = sliceSize = 100 / ($scope.dates.length -1);
+			$scope.workPeriods = [];
+			for(let event of _data.body.events){
+				year = event.date;
+				if(year == 'now')
+					year = new Date().getFullYear();
+				$scope.datesByYear["y" + year].used = true;
+				event.isAcademic = event.type == "A"
+			}
+			for(let period of _data.body.periods){
+				yFrom = period.from;
+				if(yFrom == 'now')
+					period.from = yFrom = new Date().getFullYear();
+				$scope.datesByYear["y" + yFrom].used = true;
+				yTo = period.to;
+				if(yTo == 'now')
+					period.to = yTo = new Date().getFullYear();
+				$scope.datesByYear["y" + yTo].used = true;
+				period.length = yTo - yFrom;
+				period.width = period.length * sliceSize;
+				period.isAcademic = period.type == "A"
+				if(!period.isAcademic){
+					$scope.workPeriods.push(period);
+				}
+			}
+			yTo = end;
+			for (var i = $scope.workPeriods.length - 1; i >= 0; i--) {
+				var period = $scope.workPeriods[i];
+				while(yTo > period.to){
+					$scope.workPeriods.splice(i+1, 0, {isPlaceHolder : true, length: 1, width : sliceSize})
+					yTo--;
+				}
+				yTo = period.from;
+			}
+			console.log("treated CV : ", $scope);
+		}
+		$scope.getPeriodClass = getPeriodClass;
+	}
+	function comparePeriods(_a, _b){
+		return _a.from - _b.from;
 	}
 	function compareSections(_a, _b){
 		return _a.order - _b.order;
